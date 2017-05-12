98ed6b70d068548344fe3d4d4baa400988611226
John Ryan Oliveros <johnryan.oliveros@infor.com>
2017-05-12 20:35:37 +0800

    baseline

---
+++ src/main/java/com/infor/inforavailableslots/InforAvailableSlotsApplication.java
@@ -0,0 +1,12 @@
+package com.infor.inforavailableslots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InforAvailableSlotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InforAvailableSlotsApplication.class, args);
+	}
+}
